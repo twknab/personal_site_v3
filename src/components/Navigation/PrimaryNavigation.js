eed68c19b0d542adf93b5bc56f89aa580b542004
twknab <20636750+twknab@users.noreply.github.com>
2020-08-22 02:41:06 -0700

    added react-icons to nav

--- src/components/Navigation/PrimaryNavigation.js
+++ src/components/Navigation/PrimaryNavigation.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { primaryNavItems, primaryRightSideNavItems} from './navItems.js';
+import { FcAutomatic, FcNightPortrait, FcElectricity, FcGraduationCap, FcImageFile, FcLandscape, FcLibrary, FcLightAtTheEndOfTunnel, FcMindMap, FcNook, FcReading } from "react-icons/fc";
 
-console.log(primaryNavItems, primaryRightSideNavItems);
 // TODO: Refactor menu items into the above export (primaryNavItems etc) then loop the stuff below so this component is cleaner
+// import { primaryNavItems, primaryRightSideNavItems} from './navItems.js';
+// console.log(primaryNavItems, primaryRightSideNavItems);
 
 
 function PrimaryNavigation() {
@@ -13,6 +14,8 @@ function PrimaryNavigation() {
     setActiveItem(itemName);
     // TODO: Navigate to item.
   };
+  const navAboutDropdownTitle = <span><FcMindMap className="primary-nav-icon"/> About</span>;
+  const navInterestsDropdownTitle = <span><FcElectricity className="primary-nav-icon"/> Interests</span>;
 
   return (
     <div>
@@ -30,29 +33,32 @@ function PrimaryNavigation() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link onClick={() => handleNavClick('home')} active={activeItem === 'home'}>Home</Nav.Link>
-              <NavDropdown title="About" id="collasible-nav-dropdown-about">
-                <NavDropdown.Item onClick={() => handleNavClick('skills')} active={activeItem === 'skills'}>Skills</NavDropdown.Item>
-                <NavDropdown.Item onClick={() => handleNavClick('projects')} active={activeItem === 'projects'}>Projects</NavDropdown.Item>
-                <NavDropdown.Item onClick={() => handleNavClick('education')} active={activeItem === 'education'}>Education</NavDropdown.Item>
+              <Nav.Link onClick={() => handleNavClick('home')} active={activeItem === 'home'}>
+                <FcLibrary className="primary-nav-icon"/>
+                Home
+              </Nav.Link>
+              <NavDropdown title={navAboutDropdownTitle} id="collasible-nav-dropdown-about">
+                <NavDropdown.Item onClick={() => handleNavClick('skills')} active={activeItem === 'skills'}><FcAutomatic className="primary-nav-icon" />Skills</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleNavClick('projects')} active={activeItem === 'projects'}><FcLightAtTheEndOfTunnel className="primary-nav-icon" />Projects</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleNavClick('education')} active={activeItem === 'education'}><FcGraduationCap className="primary-nav-icon" />Education</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <Container fluid>
                   <Row center>
                     <Col>
-                      <NavDropdown.Item onClick={() => handleNavClick('downloadResume')} active={activeItem === 'downloadResume'}>Download Resume</NavDropdown.Item>
+                      <NavDropdown.Item onClick={() => handleNavClick('downloadResume')} active={activeItem === 'downloadResume'}><FcNightPortrait className="primary-nav-icon"/>Download Resume</NavDropdown.Item>
                     </Col>
                   </Row>
                 </Container>
               </NavDropdown>
-              <NavDropdown title="Interests" id="collasible-nav-dropdown-interests">
-                <NavDropdown.Item onClick={() => handleNavClick('nature')} active={activeItem === 'nature'}>Nature</NavDropdown.Item>
-                <NavDropdown.Item onClick={() => handleNavClick('drawing')} active={activeItem === 'drawing'}>Drawing</NavDropdown.Item>
+              <NavDropdown title={navInterestsDropdownTitle} id="collasible-nav-dropdown-interests">
+                <NavDropdown.Item onClick={() => handleNavClick('nature')} active={activeItem === 'nature'}><FcLandscape className="primary-nav-icon"/>Nature</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleNavClick('drawing')} active={activeItem === 'drawing'}><FcImageFile className="primary-nav-icon"/>Drawing</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link onClick={() => handleNavClick('blog')} active={activeItem === 'blog'}>Blog</Nav.Link>
+              <Nav.Link onClick={() => handleNavClick('blog')} active={activeItem === 'blog'}><FcReading className="primary-nav-icon" />Blog</Nav.Link>
               <Nav.Link eventKey={2} onClick={() => handleNavClick('contact')} active={activeItem === 'contact'}>
-                Contact
+                <FcNook className="primary-nav-icon" />Contact
         </Nav.Link>
             </Nav>
           </Navbar.Collapse>
