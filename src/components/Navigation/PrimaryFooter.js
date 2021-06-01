366bc50f01c75f83d548a2801c6f524f97aa1ff2
twknab <20636750+twknab@users.noreply.github.com>
2021-05-31 20:09:33 -0700

    added basic roughed out footer

--- src/components/Navigation/PrimaryFooter.js
+++ src/components/Navigation/PrimaryFooter.js
@@ -5,9 +5,10 @@ function PrimaryFooter() {
 
   return (
     <div>
-      <Container fluid className="footer">
+      <Container fluid="true" className="pl-0 pr-0 h-100 container-fluid footer">
         <Row>
-          <Col lg>Footer</Col>
+          <Col lg><p className="handcrafted-tag">Handcrafted with care in Washington State <span role="img" aria-label="Washington State">🌲</span></p>
+          </Col>
         </Row>
       </Container>
     </div>
