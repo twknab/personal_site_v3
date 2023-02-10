3e365f61531e08d4e4b3dc6c69425c46a3ec1f2d
twknab <20636750+twknab@users.noreply.github.com>
2023-02-10 00:33:42 -0800

    add footer test

--- src/components/nav/PrimaryFooter.test.js
+++ src/components/nav/PrimaryFooter.test.js
@@ -10,6 +10,8 @@ describe("PrimaryFooter", () => {
   it("successfully renders primary footer", () => {
     const footer = screen.getByTestId("primary-footer");
     expect(footer).toHaveTextContent("handcrafted with 💚");
-    expect(footer).toHaveTextContent(`${new Date().getFullYear()} timknab.dev`);
+    expect(footer).toHaveTextContent(
+      `${new Date().getFullYear()} timknab.dev`
+    );
   });
 });
