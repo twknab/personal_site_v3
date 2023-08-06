8e10fd904c9ffc4c1cb64be4409a7b56866d0246
twknab <20636750+twknab@users.noreply.github.com>
2023-08-06 00:05:41 -0700

    cleanup and styling fixes

--- src/components/main/Experience.js
+++ src/components/main/Experience.js
@@ -8,47 +8,48 @@ const Element = Scroll.Element;
 function Experience() {
   return (
     <div>
-      <Element name="experience"></Element>
+      <Element nameName="experience"></Element>
       <Row className="experience">
+        <h1>
+          <span role="img" aria-label="about">
+            🏋️‍♂️
+          </span>{" "}
+          experience
+        </h1>
         <Col lg>
-          <h1>
-            <span role="img" aria-label="about">
-              🏋️‍♂️
-            </span>{" "}
-            experience
-          </h1>
-          <p>
-            I've been working with computers and building websites since getting
-            the internet hooked up to our{" "}
-            <a
-              href="https://duckduckgo.com/?t=ffab&q=Gateway+2000+P5-133&iax=images&ia=images"
-              target="new"
-            >
-              Gateway 2000 P5-133
-            </a>{" "}
-            's Telepath 28.8K modem. I love being creative and believe that
-            being a developer is a life-long learning journey full of challenges
-            and growth.
+          {experiences.map((experience) => (
+            <div className="experience-block">
+              <h3>{experience.jobTitle}</h3>
+              <p>
+                <b>
+                  {experience.startYear}-{experience.endYear}
+                </b>{" "}
+                -{" "}
+                <em>
+                  <b>{experience.companyName}</b>
+                </em>
+                , {experience.city}, {experience.stateAbbreviation}
+              </p>
+            </div>
+          ))}
+        </Col>
+        <Col lg className="experience-description-wrapper">
+          <p className="experience-description">
+            💠{" "}
+            <em>
+              I've been working with computers and building websites since
+              getting the internet hooked up to our{" "}
+              <a
+                href="https://duckduckgo.com/?t=ffab&q=Gateway+2000+P5-133&iax=images&ia=images"
+                target="new"
+              >
+                Gateway 2000 P5-133
+              </a>{" "}
+              's Telepath 28.8K modem. I love being creative and believe that
+              being a developer is a life-long learning journey full of
+              challenges and growth.
+            </em>
           </p>
-          <Row class="experience-content-wrapper">
-            <Col lg>
-              {experiences.map((experience) => (
-                <div class="experience-block">
-                  <h3>{experience.jobTitle}</h3>
-                  <p>
-                    <b>
-                      {experience.startYear}-{experience.endYear}
-                    </b>{" "}
-                    -{" "}
-                    <em>
-                      <b>{experience.companyName}</b>
-                    </em>
-                    , {experience.city}, {experience.stateAbbreviation}
-                  </p>
-                </div>
-              ))}
-            </Col>
-          </Row>
         </Col>
       </Row>
     </div>
--- src/components/main/Skills.js
+++ src/components/main/Skills.js
@@ -22,7 +22,7 @@ function Skills() {
             </span>{" "}
             Skills
           </h1>
-          <Row>
+          <Row className="skills-wrapper">
             {chunkedSkills.map((chunk, idx) => (
               <Col lg>
                 <ul key={idx}>
@@ -32,30 +32,7 @@ function Skills() {
                 </ul>
               </Col>
             ))}
-            {/* <Col lg>
-              <ul>
-                {skills.map((skill) => (
-                  <li key={skill}>
-                    <p>{skill}</p>
-                  </li>
-                ))}
-              </ul>
-            </Col> */}
           </Row>
-          {/* <p>
-            Agile Software Development, APIs, Asana Software, AWS, Browser
-            Compatibility Testing, Circle CI, Code Review, Coding,
-            Collaboration, Confluence, CSS3, Data Modeling, Detail Oriented,
-            Docker, End To End Testing, Estimation, Express.js, Figma, Git,
-            GitHub, GraphQL, Guru, HTML5, Integration Testing, JavaScript, Jest,
-            Jira, Material UI, MongoDB, MVC Architecture, MySQL, Node.js,
-            On-Call Rotation, Organization, Passionate, PHP, Pixel-Perfect
-            Components, Positive Attitude, Postgres, Problem Solving, Python,
-            React Testing Library, React.js, RSpec, Ruby, Ruby on Rails, Ruby
-            Version Manager, SCSS, Slack, Software Development Life Cycle, Team
-            Development, Team Player, Technical Documentation, Time Management,
-            Triaging, TypeScript, Unit Testing, Vue.js, Web Applications
-          </p> */}
         </Col>
       </Row>
     </div>
