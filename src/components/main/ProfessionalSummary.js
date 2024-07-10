edd92f0e9057239fab79542e1fdbe1631ea17c52
twknab <20636750+twknab@users.noreply.github.com>
2024-07-09 22:27:14 -0700

    refactor: Updated specialization skills in ProfessionalSummary

--- src/components/main/ProfessionalSummary.js
+++ src/components/main/ProfessionalSummary.js
@@ -28,22 +28,22 @@ function ProfessionalSummary() {
           <p className="light-text-shadow">
             My name is Tim. I'm a{" "}
             <b className="highlighter">Full Stack Software Engineer</b>,
-            specializing in: <b className="highlighter">Node.js/Express.js</b>,{" "}
+            specializing in: <b className="highlighter">Ruby on Rails</b>,{" "}
             <b className="highlighter">React.js</b>,{" "}
             <b className="highlighter">Vue.js</b>,{" "}
             <b className="highlighter">JavaScript</b>,{" "}
-            <b className="highlighter">Ruby on Rails</b>,{" "}
+            <b className="highlighter">Node.js/Express.js</b>,{" "}
             <b className="highlighter">MongoDB</b> /{" "}
             <b className="highlighter">MySQL</b> /{" "}
-            <b className="highlighter">Postgres</b> databases. I'm a{" "}
+            <b className="highlighter">PostgreSQL</b> databases. I'm a{" "}
             <b className="highlighter">motivated</b>{" "}
             <b className="highlighter">team-player</b>, comfortable with all
             aspects of the{" "}
             <b className="highlighter">Software Development Life Cycle</b>. I
             continually seek to expand my technical expertise and stay on top of
             current trends and the latest technologies. I care deeply about my
-            <b className="highlighter">users</b> and bringing{" "}
-            <b className="highlighter">strong value to my team</b>.
+            <b className="highlighter">users</b>, polished digital experiences
+            and bringing <b className="highlighter">strong value to my team</b>.
           </p>
           <Row className="socials">
             <Col
