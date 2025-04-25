529876fbdc8121d903bad03857d097fb9710bec0
Tim Knab <20636750+twknab@users.noreply.github.com>
2025-04-24 21:26:39 -0700

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -14,7 +14,6 @@ function MainContent() {
     <div>
       {/* TODO: Delete the max container height when reverting to the original */}
       <Container style={{height: "100vh"}} fluid="true" className="mainContent" data-testid="main-content">
-        // <h1>🔧</h1>
         <ProfessionalSummary />
         <AboutMe />
         <Skills />
