eaa94b769e5ecf78d424c81992c9a814840935d0
Tim Knab <20636750+twknab@users.noreply.github.com>
2025-05-03 16:09:02 -0700

    Update ProfessionalSummary.js

--- src/components/main/ProfessionalSummary.js
+++ src/components/main/ProfessionalSummary.js
@@ -28,8 +28,10 @@ function ProfessionalSummary() {
           <p className="light-text-shadow">
             My name is Tim. I'm a{" "}
             <b className="highlighter">Senior Full Stack Software Engineer</b>,
-            specializing in: <b className="highlighter">Node.js/Express.js</b>,{" "}
-            <b className="highlighter">TypeScript / JavaScript</b>,{" "}
+            specializing in: <b className="highlighter">Node.js</b> /{" "}
+            <b className="highlighter">Express.js</b>,{" "}
+            <b className="highlighter">TypeScript</b> /{" "}
+            <b className="highlighter">JavaScript</b>,{" "}
             <b className="highlighter">React.js</b>,{" "}
             <b className="highlighter">Python</b>,{" "}
             <b className="highlighter">Ruby on Rails</b>,{" "}
